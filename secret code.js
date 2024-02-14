e207f22e0517eaadc68a03609a5fd864e332eb4d
umbraweb <159861347+umbraweb@users.noreply.github.com>
2024-02-14 14:37:42 -0800

    Add files via upload

---
+++ secret code.js
@@ -0,0 +1,7 @@
+//hehehe can you find this
+console.log(
+  "this is about Japan,and about the Christian oppression in Japan,and I chose this to relate it to the current oppression of children"
+);
+const sym1 = Symbol.for();
+const sym2 = symbol("foo");
+const sym3 = Symbol("fuji");
