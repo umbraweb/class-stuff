9f648c3800ec33611bbe22c5341e241c7dce0bc2
umbraweb <jamessgaskill@gmail.com>
2024-02-15 03:35:44 -0800

    Update secret code.js

--- secret code.js
+++ secret code.js
@@ -2,6 +2,3 @@
 console.log(
   "this is about Japan,and about the Christian oppression in Japan,and I chose this to relate it to the current oppression of children"
 );
-const sym1 = Symbol.for();
-const sym2 = symbol("foo");
-const sym3 = Symbol("fuji");
