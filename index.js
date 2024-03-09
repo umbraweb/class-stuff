32059a66e502135c5bd33285a8788e03c3dc969e
James <jamessgaskill@gmail.com>
2024-03-08 18:35:22 -0800

    puts on page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //information about the ammendments
 let amendment = prompt("Which amendment would you like to learn about?\n");
-let Ammendments = document.getelementbyid("ammendments")
+let Ammendments = document.getElementById("ammendments")
 if (amendment === "1") {
   console.log(
     "Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances."
